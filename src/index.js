b9fdc8fd4d551ae909a3992086f283a8ae6fb8a2
HydroxideX <Yahiaelsaadawy@gmail.com>
2020-07-19 17:10:47 +0200

    structuring

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Welcome from './Components/ButtonsBar/RunButton.js'
+
+
+
+/*class RunButton extends React.Component {
+  constructor (props) {
+
+  }
+
+  handleClick() {
+
+  }
+}
+
+class RunButton extends React.Component {
+  constructor (props) {
+
+  }
+
+  handleClick() {
+
+  }
+}
+*/
 
 
 
 ReactDOM.render(
-  < />,
+  <Welcome />,
   document.getElementById('root')
 );
