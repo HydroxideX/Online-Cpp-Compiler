9acc75aaf2123214ab0fa166dcd34240e6106104
sh3ra <youssefalshaarawi@yahoo.com>
2020-07-20 11:59:10 +0200

    editor width

--- src/Components/MainArea/MainArea.js
+++ src/Components/MainArea/MainArea.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import AceEditor from "react-ace";
-import "brace/mode/javascript";
-import "brace/theme/monokai";
+import "brace/mode/c_cpp";
+import "brace/theme/solarized_dark";
 class MainArea extends Component {
   render() {
     return (
-      <div>
-        <AceEditor mode="javascript" theme="monokai" zzz/>
+      <div
+        style={{
+          border: "2px solid red",
+          marginLeft:"100px",
+          marginRight:"100px"
+        }}
+      >
+        <AceEditor width="100%" fontSize="14pt" mode="c_cpp" theme="solarized_dark" showPrintMargin={false} zzz />
       </div>
     );
   }
